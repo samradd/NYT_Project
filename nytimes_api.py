127eb04c2f125328bbf00123d0ec56c64e2a8b44
hgoldstein06 <hgoldstein06@gmail.com>
2014-10-28 19:41:06 -0400

    Work Session 10/28
    We printed the number of pages! AND divided by 10

--- nytimes_api.py
+++ nytimes_api.py
@@ -9,6 +9,48 @@ print (r.status_code)
 
 data = json.loads(r.text)
 
+for first_layer in data:
+	# print (first_layer)
+
+	if first_layer == 'response':
+		
+		for second_layer in data[first_layer]:
+			# print (second_layer)
+
+			count = data['response']['meta']['hits']
+			# print (count)
+
+			pages = count / 10
+
+			# print (pages)
+
+
+			for pages in data:
+				payload = {'begin_date':'19000101', 'api-key': '7814e2003e284eebd3d2c5248733ece8:17:65376813'}
+				data = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json', params=payload)
+
+			
+			# 	for first_layer in data:
+					# print (first_layer)
+
+					# if first_layer == 'response':
+
+					# 	for second_layer in data[first_layer]:
+					# 		# print (second_layer)
+
+					# 		if second_layer == 'docs':
+
+					# 			for third_layer in data[first_layer][second_layer]:
+					# 				print (third_layer['headline']['main'])
+
+
+
+				
+
+
+
+
+			
 #get to offset & divide 13million number by 10, then loop through for that num of pages
 # for page in xnum_of_pages
 
@@ -21,7 +63,7 @@ data = json.loads(r.text)
 # 	r = requests...
 
 
-print (json.dumps(data, indent=4))
+# print (json.dumps(data, indent=4))
 
 
 # 
